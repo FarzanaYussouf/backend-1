1888e7289957f03a7e81d7af37bef6c68d18717e
farzana yussouf <tvs80649@gmail.com>
2024-08-21 00:46:17 +0500

    last

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
-const Connection = async()=>{
-    try{
-        await mongoose.connect("mongodb+srv://farzanayussouf:billmughal911@cluster0.d9fs5kc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    }catch(error){
+const Connection = async () => {
+    try {
+      
+        await mongoose.connect("mongodb+srv://backend-node:pakistan@cluster0.5yf37or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("Database connected");
+        
+    } catch (error) {
         console.log(error);
+        
     }
 }
-export default Connection;
+
+export default Connection
--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-import express from "express";
-const app =express();
-import  Connection  from "./db/conn.js";
-import dotenv from "dotenv"
 
+
+import express from "express";
+const app = express();
+import dotenv from "dotenv"
+import Connection from "./db/conn.js";
+import buyerRouter from "./routes/buyers/buyerRoutes.js";
+import cors from "cors"
+import sellerRouter from "./routes/sellers/sellerRoutes.js";
+
+
+const port = 8000;
 dotenv.config()
-const PORT= process.env.PORT;
+app.use(express.json())
+app.use(cors())
+
 Connection()
-app.listen(PORT,()=>{
-    console.log("backend is running")
+
+app.use("/api/buyers/", buyerRouter);
+app.use("/api/sellers", sellerRouter)
+app.listen(port, () => {
+    console.log("Listening on 8000");
 })
+
+// mongodb+srv://affaq020:Affaqkhan@cluster0.x9eg3.mongodb.net/lab
+
+
+
+
+    
